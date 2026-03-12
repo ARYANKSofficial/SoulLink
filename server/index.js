d62539364750fae7fd417026ef305421273c20f2
Your Name <you@example.com>
2026-03-12 20:00:58 +0530

    Fix media_status handler formatting

--- server/index.js
+++ server/index.js
@@ -122,7 +122,13 @@ io.on('connection', (socket) => {
         io.to(data.to).emit('ice-candidate', data);
     });
 
-    socket.on('media_status', (data) => {\r\n        if (data && data.roomId) {\r\n            io.to(data.roomId).emit('media_status', data);\r\n        }\r\n    });\r\n\r\n    socket.on('send_message', (data) => {
+    socket.on('media_status', (data) => {
+        if (data && data.roomId) {
+            io.to(data.roomId).emit('media_status', data);
+        }
+    });
+
+    socket.on('send_message', (data) => {
         // data: { roomId, text, ... }
         if (data.roomId) {
             io.to(data.roomId).emit('receive_message', data);
@@ -138,5 +144,7 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
+
+
 
 
